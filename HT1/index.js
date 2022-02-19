732ca5642c9091c42cfdcee84b2c33171813b5ed
KalininaOI <kalininanana@gmail.com>
2022-02-19 11:47:21 +0300

    L1

---
+++ HT1/index.js
@@ -0,0 +1,7 @@
+const ourPlanet = "Earth"; // название платены
+let nowDateUser; // текущее время пользователя
+let articleCount; // количество статей
+const isCashPayment; // отплата наличными? да? нет?
+let surname; // фамилия
+let firstName; // имя
+let secondName; // отчество
