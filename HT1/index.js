ccac3a1e7f02eb6620b27e6d6ab6043d362a6baa
KalininaOI <kalininanana@gmail.com>
2022-02-23 14:04:58 +0300

    переменная

--- HT1/index.js
+++ HT1/index.js
@@ -1,7 +1,7 @@
 const ourPlanet = "Earth"; // название платены
 let nowDateUser; // текущее время пользователя
 let articleCount; // количество статей
-const isCashPayment; // отплата наличными? да? нет?
+let isCashPayment; // отплата наличными? да? нет?
 let surname; // фамилия
 let firstName; // имя
 let secondName; // отчество
