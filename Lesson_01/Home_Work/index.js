6420784eac882ba1aa4df8e42d8971ad5591d039
KalininaOI <kalininanana@gmail.com>
2022-02-25 21:35:45 +0300

    HW01

--- Lesson_01/Home_Work/index.js
+++ Lesson_01/Home_Work/index.js
@@ -2,6 +2,6 @@ const ourPlanet = "Earth"; // название платены
 let nowDateUser; // текущее время пользователя
 let articleCount; // количество статей
 let isCashPayment; // отплата наличными? да? нет?
-let surname; // фамилия
+let surname; // фамилия - закон разрешает менять ФИО, так что это все будет let
 let firstName; // имя
 let secondName; // отчество
